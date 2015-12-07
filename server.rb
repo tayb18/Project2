6086856d225f326fb51573ab8e94fe0dcb35f6f1
Basso Basso <taylorpbasso@gmail.com>
2015-12-07 18:43:08 -0500

    migrations heroku strugz

--- server.rb
+++ server.rb
@@ -43,7 +43,7 @@ module App
       @user = User.find(session[:user_id])
       @articles = Article.all
       article = Article.find_by({id: params[:id]})
-      # @user = User.find(session[:user_id]) if session[:user_id])
+
       erb :homepage
     end
 
@@ -59,10 +59,11 @@ module App
     end
 
     post "/new_article" do
-      category = Category.find_by(name: params[:name]) || Category.create({name: params[:name]})
-      article = Article.create({title: params[:title], content: params[:content], user_id: session[:user_id]})
+      category = Category.find_by(id: params[:id]) || Category.create({name: params[:name]})
+      article = Article.create({title: params[:title], date_created: DateTime.now ,content: params[:content], user_id: session[:user_id]})
       article.categories.push(category)
       article.save
+
       redirect to "/homepage"
     end
 
@@ -70,8 +71,8 @@ module App
       redirect to "/" if !session[:user_id]
       @user = User.find(session[:user_id])
       @this_article = Article.find(params[:id])
-      @find_author = User.find(@this_article[:user_id])
       @category = Category.find(params[:id])
+      @find_author = User.find(@this_article.user_id)
 
       erb :article
     end
@@ -79,6 +80,7 @@ module App
     get "/articles/:id/update_article" do
       @user = User.find(session[:user_id])
       @article = Article.find(params[:id])
+      @category = Category.find(params[:id])
       erb :update_article 
     end
 
@@ -88,6 +90,27 @@ module App
       article = Article.find(params[:id])
       article.update({title: params[:title], content: params[:content]})
       redirect to "/homepage"
+    end
+
+    get "/articles/:id/add_category" do
+      redirect to "/" if !session[:user_id]
+      @user = User.find(session[:user_id])
+      @this_article = Article.find(params[:id])
+      @category = Category.find(params[:id])
+      @find_author = User.find(@this_article.user_id)
+
+      erb :add_category 
+    end
+
+    post "/articles/:id/add_category" do
+      @user = User.find(session[:user_id])
+      @this_article = Article.find(params[:id])
+      category = Category.find_by(id: params[:id]) || Category.create({name: params[:name]})
+      article = Article.find_by(id: params[:id])
+      article.categories.push(category)
+      article.save
+
+      redirect to "/articles/:id"
     end
 
   end #class
